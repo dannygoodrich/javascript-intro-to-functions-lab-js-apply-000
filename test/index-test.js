dc0a24c97bd36da5094d878ae5fece0f13e710c5
Danny Goodrich <dannyngoodrich@gmail.com>
2019-11-10 00:08:22 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -13,11 +13,11 @@ describe('whisper(string)', function() {
 
 describe('logShout(string)', function() {
   it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
+    const s = expect.spyOn(console, 'log').andCallThrough()
 
     logShout('hello')
 
-    expect(spy).toHaveBeenCalledWith('HELLO')
+    expect(s).toHaveBeenCalledWith('HELLO')
 
     console.log.restore()
   })
