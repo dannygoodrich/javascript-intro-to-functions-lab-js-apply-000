794d02668a1b785eeddd9839eb3a7c38633c3bed
Danny Goodrich <dannyngoodrich@gmail.com>
2019-11-10 01:15:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ function logWhisper(s) {
 
 function sayHiToGrandma(s) {
   if (s === whisper(s)) {
-  return ("I can't hear you!");
+  return "I can't hear you!";
 } else if (s === shout(s)) {  
-  return ("YES INDEED");
+  return "YES INDEED";
 } else {
-return ("I love you, too.");
+return "I love you, too.";
 }
 }
 
